2ca28d78649870f1001c008c68804e0e3d347585
STENTORS <ezraibaldwin@gmail.com>
2025-08-13 15:21:37 +0100

    functional login system

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 import pandas as pd
 import altair as alt
-
 from datetime import datetime, timedelta
+import os
+import hmac
+from dotenv import load_dotenv
+load_dotenv()
 
+#===========Sheet Connections===========
 # Connect to the public sheet
 social_conn = st.connection("social_gsheets", type=GSheetsConnection)
 social_df = social_conn.read()
@@ -30,17 +34,64 @@ def normalize_dates(date_str):
         except Exception:
             return None
 
-social_df["Date"] = social_df["Date"].apply(normalize_dates)
 
+def check_password():
+    """Returns `True` if the user had a correct password."""
+
+    def login_form():
+        """Form to collect username & password"""
+        with st.form("Credentials"):
+            st.text_input("Username", key="username")
+            st.text_input("Password", type="password", key="password")
+            st.form_submit_button("Log in", on_click=password_entered)
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if os.environ.get(f"{st.session_state["username"].upper()}_STREAMLIT_PASSWORD") \
+            and hmac.compare_digest(
+            st.session_state["password"],
+            os.environ.get(f"{st.session_state["username"].upper()}_STREAMLIT_PASSWORD")
+        ):
+            st.session_state["password_correct"] = True
+            # Don't store the username or password in the session
+            del st.session_state["password"]
+            del st.session_state["username"]
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the username + password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show inputs for username + password.
+    login_form()
+    if "password_correct" in st.session_state:
+        st.error("😕 Incorrrect Admin Credentials")
+    return False
+
+
+#===========Main Application===========
+social_df["Date"] = social_df["Date"].apply(normalize_dates)
 
 #========Header========
 st.logo("lpd-logo.png", size="large")
-st.title("Linguistpd Sales Analytics")
+st.title("Linguistpd Admin Dashboard")
 
+#========login system========
+if not check_password():
+    st.stop()
 
 # ========Content========
-tab_main, tab_sales, tab_social = st.tabs(["📈 Analytics", "Sales Data", "Social Data"])
+tab_main, tab_sales, tab_social, tab_email = st.tabs(["📈 Analytics", "Sales Data", "Social Data", "Email Marketing"])
+
+with st.sidebar:
+    st.button("Reload Scraper")
+    st.button("Customer Sales Tracker")
+    st.button("Amalytics")
+    st.button("raw data")
+
 with tab_main:
+
     st.header("Sales x Post by Date")
 
     #==============Graph Options==============
@@ -54,7 +105,6 @@ with tab_main:
     )
 
     #==============Graph View==============
-        
     # Parse dates
     sales_df["Date and Time"] = pd.to_datetime(sales_df["Date and Time"], errors="coerce")
     sales_df["Date"] = sales_df["Date and Time"].dt.date
@@ -76,12 +126,12 @@ with tab_main:
 
     # Aggregate sales count per year
     sales_yearly = sales_filtered.groupby(
-        sales_filtered["Date and Time"].dt.year
+        sales_filtered["Date and Time"].dt.month
     ).size().reset_index(name="Sales Count")
 
     # Chart
     chart = alt.Chart(sales_yearly).mark_line(point=True).encode(
-        x=alt.X("Date and Time:O", title="Year"),
+        x=alt.X("Date and Time:O", title="Month"),
         y=alt.Y("Sales Count:Q", title="Number of Sales"),
         tooltip=["Date and Time", "Sales Count"]
     ).properties(
@@ -98,6 +148,9 @@ with tab_social:
     st.header("Social Media Data")
     st.dataframe(social_df)
 
-st.divider()
+with  tab_email:
+    st.header("Email Markting Data")
 
+
+st.divider()
 st.button("Run Scraper")
